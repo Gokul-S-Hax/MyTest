f0f47dcb1d224fdf7e2ec562f889b0682470ea87
Gokul-S-Hax <61446768+Gokul-S-Hax@users.noreply.github.com>
2021-06-06 17:09:08 +0530

    Add files via upload

--- worker.cc
+++ worker.cc
@@ -1,15 +1,31 @@
-#include <mkl.h>
-#include <hbwmalloc.h>
+#include <vector>
+#include <algorithm>
+
+//helper function , refer instructions on the lab page
+void append_vec(std::vector<long> &v1, std::vector<long> &v2) {
+  v1.insert(v1.end(),v2.begin(),v2.end());
+}
 
 
-//implement scratch buffer on HBM and compute FFTs, refer instructions on Lab page
-void runFFTs( const size_t fft_size, const size_t num_fft, MKL_Complex8 *data, DFTI_DESCRIPTOR_HANDLE *fftHandle) {
-#pragma omp parallel for
-  for(size_t i = 0; i < num_fft; i++) {
-	const long buff_size = 1000;
-	MKL_Complex8 *buff;
-	hbw_posix_memalign((void**) &buff, 4096, sizeof(MKL_Complex8)*buff_size);
-	&buff = data[i*fft_size];
-    DftiComputeForward (*fftHandle, buff);
+void filter(const long n, const long m, float *data, const float threshold, std::vector<long> &result_row_ind) {
+  
+  #pragma omp parallel for
+  for (long i = 0; i < n; i++){
+    float sum = 0.0f;
+    //compute sum of all the elements in a row
+	#pragma omp parallel for reduction(+: sum)
+    for (long j = 0; j < m; j++) {
+      sum += data[i*m + j];
+    } 
+    
+    //store the sum in an array(vector) only if it is valid (i.e if it is greater than threshold
+	#pragma omp critical
+	{if (sum > threshold) 
+      result_row_ind.push_back(i);}
+    
   }
+  
+  //sort the values stored in the vector
+  std::sort(result_row_ind.begin(),
+            result_row_ind.end());
 }
